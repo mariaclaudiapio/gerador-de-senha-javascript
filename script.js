fd39ac685de7ee718ad6be22114333477722f194
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-12 14:16:08 -0300

    Criação de funções base para a geração de senhas:
    geradoras de letras maiusculas;
    geradoras de letras minúsculas;
    geradoras de números;
    geradoras de símbolos.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "@#$%&";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
+
+generatePasswordButton.addEventListener("click", () => {
+  console.log("teste");
+});
